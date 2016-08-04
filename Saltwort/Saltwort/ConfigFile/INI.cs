c2bd65499f7544da5f655a2e7127ec4e7d044938
ubuntoid <teges.ya@yandex.ru>
2016-08-04 14:00:11 +0400

    Initial

---
+++ Saltwort/Saltwort/ConfigFile/INI.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Saltwort.ConfigFile
+{
+    public class INI
+    {
+        readonly string _path;
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern long WritePrivateProfileString(string section, string key, string value, string filePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern int GetPrivateProfileString(string section, string key, string Default, StringBuilder retVal, int size, string filePath);
+
+        public INI(string iniPath)
+        {
+            _path = new FileInfo(iniPath).FullName;
+        }
+
+        public string Read(string key, string section = null)
+        {
+            var retVal = new StringBuilder(255);
+            GetPrivateProfileString(section, key, "", retVal, 255, _path);
+            return retVal.ToString();
+        }
+
+        public void Write(string key, string value, string section = null)
+        {
+            WritePrivateProfileString(section, key, value, _path);
+        }
+
+        public void DeleteKey(string key, string section = null)
+        {
+            Write(key, null, section);
+        }
+
+        public void DeleteSection(string section = null)
+        {
+            Write(null, null, section);
+        }
+
+        public bool KeyExists(string key, string section = null)
+        {
+            return Read(key, section).Length > 0;
+        }
+    }
+}
