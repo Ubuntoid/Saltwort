e1ab8b655b74dee1a6a137a21b2b9257e6b0d1ad
ubuntoid <teges.ya@yandex.ru>
2016-08-04 14:07:57 +0400

    Добавлены функции определяющие версию системы и разрядность

---
+++ Saltwort/Saltwort/Interop/NativeMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Saltwort.Interop
+{
+    public class NativeMethods
+    {
+        [DllImport("kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool IsWow64Process(
+              [In] IntPtr hProcess,
+              [Out] out bool wow64Process
+            );
+    }
+}
---
+++ Saltwort/Saltwort/System/Info.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using Saltwort.Interop;
+
+namespace Saltwort.System
+{
+    public class Info
+    {
+        public static string OSArch
+        {
+            get
+            {
+                if (IntPtr.Size == 8 || InternalCheckIsWow64())
+                    return "X64";
+                return "X86";
+            }
+        }
+
+        public static bool IsWow64
+        {
+            get
+            {
+                if (IntPtr.Size == 8 || InternalCheckIsWow64())
+                    return true;
+                return false;
+            }
+        }
+
+        private static bool InternalCheckIsWow64()
+        {
+            if ((Environment.OSVersion.Version.Major == 5 && Environment.OSVersion.Version.Minor >= 1) ||
+                Environment.OSVersion.Version.Major >= 6)
+            {
+                using (Process p = Process.GetCurrentProcess())
+                {
+                    bool retVal;
+                    if (!NativeMethods.IsWow64Process(p.Handle, out retVal))
+                    {
+                        return false;
+                    }
+                    return retVal;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public static bool IsXPOrHigher()
+        {
+            OperatingSystem os = Environment.OSVersion;
+            return (os.Platform == PlatformID.Win32NT) && ((os.Version.Major > 5) || ((os.Version.Major == 5) && (os.Version.Minor >= 1)));
+        }
+
+        static bool IsWinVistaOrHigher()
+        {
+            OperatingSystem os = Environment.OSVersion;
+            return (os.Platform == PlatformID.Win32NT) && (os.Version.Major >= 6);
+        }
+
+    }
+}
